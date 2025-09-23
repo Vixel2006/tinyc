a29614ca5cbbcb4f3c9765b04568a2fcca2cb9b1
yushi <yusufshihata2006@gmail.com>
2025-09-23 17:10:16 +0300

    Start working on the parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tinyc::lexer::lexer::Lexer;
 
 fn main() {
-    let lexer = Lexer::new("if (i <= 0 || i == 1) { int i = 0; char y = '\t'; return 0; }");
+    let lexer = Lexer::new("if (i <= 0 || i == 1) {\nint i = 0;\nchar y = '\t';\nreturn 0;\n}");
 
     for token in lexer {
         println!("{:?}", token);
